711b8293b6b525377879c6fcf4b14b8d26c8b723
Tarunroy20 <tarunroy1820@gmail.com>
2024-08-30 20:40:51 +0530

    Add finally unaryOP.js file

---
+++ UnaryOP.js
@@ -0,0 +1,28 @@
+// Unary operator -: Agar kisi bhi number ko one se increment karna
+//      chahte hain tab use hoga waha unary operator.
+//  For Example -: year++
+
+//  1. increment karna
+// let year = 2000;
+// console.log(year++); // 2000
+
+//  1. Decrement karna
+// let year2 = 2000;
+// console.log(year2--); // 2000
+
+// Pre Increment (change, then use)
+// let age = 10;
+// let newAge = ++age;
+// console.log(newAge)
+
+// PostIncrement (use,  then change )
+// let age1 = 10;
+// let newAge1 = age1++;
+// console.log(newAge1);
+
+// Practice Question-: 1
+
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+console.log(newNum);
