14d7f5c8e56f2643c84c3548542a2986bee4a23f
Tarunroy20 <tarunroy1820@gmail.com>
2024-08-30 19:38:46 +0530

    Add the two file AssignOP & UnaryOP.js

---
+++ AssignOP.js
@@ -0,0 +1,23 @@
+// 1. Add
+
+let age = 22;
+age += 2;
+console.log(age);
+
+// 2. Sub
+
+let age1 = 22;
+age1 -= 2;
+console.log(age1);
+
+// 3.Mul
+
+let age2 = 22;
+age2 *= 2;
+console.log(age2);
+
+// 4. Div
+
+let age4 = 22;
+age4 /= 2;
+console.log(age4);
