fc56741693574c97327fa1c508a9cddeb5592cb3
Tarunroy20 <tarunroy1820@gmail.com>
2024-08-30 14:31:13 +0530

    add modulo and power  in operation

--- operation.js
+++ operation.js
@@ -1,4 +1,42 @@
+// Addition Operation;
 var a = 20;
 var b = 45;
 var sum = a + b;
 console.log("Sum of a and b is:", sum);
+
+// Substraction operation;
+
+var a = 20;
+var b = 45;
+var sub = b -a;
+console.log("sub of b and a is:", sub);
+
+
+// Multiplication operation;
+
+var a = 2;
+var b = 4;
+var mul = b * a;
+console.log("mul of b and a is:", mul);
+
+
+// Division operation;
+
+var b = 2;
+var a = 4;
+var div = b/a;
+console.log("div of b and a is:", div);
+
+
+// Modulus operation;(Remainder operation);
+var a= 12;
+var b = 5;
+var modulo = a%b;
+console.log("modulo of b and a is:", modulo);
+
+// Exponentiation operation;(Power operation);
+
+var a= 2;
+var b = 3;
+var pow = a**b;
+console.log("pow of b and a is:", pow);
