397e558bc06261b77044b38cca735cbdcc0d9abc
Tarunroy20 <tarunroy1820@gmail.com>
2024-08-30 15:17:42 +0530

    Add the new file oprpre & prac1.js

---
+++ NaN.js
@@ -0,0 +1,9 @@
+// NaN => Not a Number
+    // Its a basically type of number but it do not represent any  number / its represent he invalid number.
+    // Such a 1 = 1 one is represent the number to be one but NaN don't represent any kind of number./its represent he invalid number.
+
+
+var a = NaN;
+var b = NaN
+var c = a+b;
+console.log(c);
---
+++ oprpre.js
@@ -0,0 +1,23 @@
+// Its mathematical way to solve your any long math expressions. using this BOADMAS Rule.
+// 1.
+var a = 2;
+var b = 1;
+var c = 3;
+var total = (a + b) * c;
+console.log(total);
+
+// 2.
+var a = 3;
+var b = 1;
+var c = 2;
+var d = 2;
+var total = a / b + c ** d;
+console.log(total);
+
+// 3.
+var a = 4;
+var b = 1;
+var c = 6;
+var d = 2;
+var total = a + b * c/ d;
+console.log(total);
